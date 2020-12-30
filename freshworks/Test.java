cc61d621f415d2979d71be1086cc235cfd2f6e17
vinaygoel24 <68969921+vinaygoel24@users.noreply.github.com>
2020-12-30 22:45:54 +0530

    added code folder

---
+++ freshworks/Test.java
@@ -0,0 +1,29 @@
+package freshworks;
+
+@SuppressWarnings("serial")
+class DuplicateKeyFound extends Exception {
+
+}
+@SuppressWarnings("serial")
+class WrongKey extends Exception {
+
+}
+@SuppressWarnings("serial")
+class KeySizeReached extends Exception{
+	
+}
+@SuppressWarnings("serial")
+class TimeExceeded extends Exception{
+	
+}
+@SuppressWarnings("serial")
+class ValueSizeReached extends Exception{
+	
+}
+public class Test
+ {
+ public static void main(String s[])
+ {
+	 
+ }
+}
